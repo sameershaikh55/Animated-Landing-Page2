631c00d54ba59427cbc23d49543e774620ef5a5d
sameer-shaikh <sameermubeen4@gmail.com>
2021-04-04 23:31:27 +0500

    Hover Effect Added on services Cards Image

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -5,7 +5,18 @@ const ServiceCard = ({ title, img, description, classN }) => {
 	return (
 		<div className="serviceCard_container">
 			<div className="img_container">
-				<img src={img} alt={img} />
+				<figure class="snip0023">
+					<img src={img} alt="sample26" />
+					<div>
+						<div class="curl"></div>
+						<div class="text">
+							<div>
+								<h4>{title}</h4>
+							</div>
+						</div>
+					</div>
+				</figure>
+				{/* <img src={img} alt={img} /> */}
 			</div>
 			<div className={`body_text ${classN}`}>
 				<h3> {title} </h3>
